77f19b8c0a5896b3321e3e2853bb9b19413b1e7a
Adam <adamsembiring01@gmail.com>
2025-03-20 12:10:58 +0700

    done milestone5

--- src/main.rs
+++ src/main.rs
@@ -2,58 +2,42 @@ use std::{
     fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
-    thread, 
+    thread,
     time::Duration,
 };
 use advprog_modul6::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    let pool = ThreadPool::new(8);
+    let pool: ThreadPool = ThreadPool::new(4);
 
-    for stream in listener.incoming(){
+    for stream in listener.incoming() {
         let stream = stream.unwrap();
-        
-        pool.execute( || {
+
+        pool.execute(|| {
             handle_connection(stream)
-        });
+        }); 
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-   
-    let http_request : Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
-    
-    if let Some(first_line) = http_request.first() {
-        let parts: Vec<&str> = first_line.split_whitespace().collect();
+    let buf_reader = BufReader::new(&stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-        if parts.len() > 1 {
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "static/hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(10));
+            ("HTTP/1.1 200 OK", "static/hello.html")
+        },
+        _ => ("HTTP/1.1 404 NOT FOUND", "static/404.html"),
+    };
 
-            let path = parts[1];
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
 
-            println!("Requested path: {}", path);
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
-            let (status_line, filename) = match path {
-                "/" => ("HTTP/1.1 200 OK", "static/hello.html"),
-                "/sleep" => {
-                    thread::sleep(Duration::from_secs(10));
-                    ("HTTP/1.1 200 OK", "static/hello.html")
-                }
-                _ => ("HTTP/1.1 404 NOT FOUND", "static/404.html"),
-            };
-
-            let contents = fs::read_to_string(filename).unwrap();
-
-            let length = contents.len();
-    
-            let response = format!("{status_line}\r\nContent-Length:{length}\r\n\r\n{contents}"); 
-
-            stream.write_all(response.as_bytes()).unwrap();
-        }
-    }
+    stream.write_all(response.as_bytes()).unwrap();
 }
